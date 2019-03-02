515443238d5613139fdb4ef7fa8b7bf3deb42068
Spencer Brooks <spencer@databit.io>
2019-03-02 10:55:41 -0700

    initial commit convert to react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+// import './App.css'
+import io from 'socket.io-client'
 
 class App extends Component {
-  render() {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: '',
+      inMessages: [],
+    }
+    this.socket = io('localhost:3000')
+
+    this.socket.on('chat message', (msg) => {
+      this.setState({ inMessages: [...this.state.inMessages, msg] })
+    })
+
+    this.submit = (e) => {
+      e.preventDefault()
+      this.socket.emit('chat message', this.state.message)
+      this.setState({ message: '' })
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({ message: e.target.value})
+  } 
+  
+  currMessages = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          {this.state.inMessages.map((msg, index) => {
+            return <li key={index}>{msg}</li>
+          })}
+        </ul>
       </div>
-    );
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.inMessages.length === 0 ? 
+          <div>No Messages</div> :
+          this.currMessages() 
+        }
+
+        <form action='' onSubmit={this.submit}>
+          <input id='m' onChange={this.onChange} value={this.state.message}/><button>Send</button>
+        </form>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
