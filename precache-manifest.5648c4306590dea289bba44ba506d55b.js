0a6671621e21d425b71435eece8c105988785ce2
Spencer Brooks <spencer@databit.io>
2019-03-06 13:14:48 -0700

    Updates

---
+++ precache-manifest.5648c4306590dea289bba44ba506d55b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/chatterclient/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "355bae638a2136080eb6",
+    "url": "/chatterclient/static/css/main.188f8f18.chunk.css"
+  },
+  {
+    "revision": "05a35bfe2c771f5db6de",
+    "url": "/chatterclient/static/js/runtime~main.05a35bfe.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/chatterclient/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "e059e7f16402ba586600",
+    "url": "/chatterclient/static/js/2.e059e7f1.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/chatterclient/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/chatterclient/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/chatterclient/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/chatterclient/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/chatterclient/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/chatterclient/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "355bae638a2136080eb6",
+    "url": "/chatterclient/static/js/main.355bae63.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/chatterclient/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/chatterclient/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/chatterclient/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/chatterclient/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/chatterclient/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/chatterclient/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/chatterclient/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/chatterclient/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "e059e7f16402ba586600",
+    "url": "/chatterclient/static/css/2.63a30ecd.chunk.css"
+  },
+  {
+    "revision": "142365b57a413539451102a677fba6dc",
+    "url": "/chatterclient/index.html"
+  }
+];
