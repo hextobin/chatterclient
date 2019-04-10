ab8100a3702f5aab61b6c485abb6f0c2a9139e7b
Spencer Brooks <spencer@databit.io>
2019-04-09 20:31:17 -0600

    add user test

--- cypress/integration/chat_test.spec.js
+++ cypress/integration/chat_test.spec.js
@@ -1,13 +1,28 @@
 describe('Creates a User', () => {
 
+  const userName = "AliceBobEve"
+
   before(() => {
     cy.visit('http://localhost:3000')
   })
 
   it('Enters Username', () => {
     cy.get('#userbox')
-      .type("Bob")
-      .should('have.value', "Bob")
+      .type(userName)
+      .should('have.value', userName)
+  })
+
+  it('Creates a User', () => {
+    cy.server()
+    cy.route('POST', '/socket.io/**').as('enterUser')
+    cy.get('#userbox')
       .type('{enter}')
+    cy.wait('@enterUser').then((xhr) => {
+      cy.get('.ui.card').last().within(() => {
+        cy.get('.user-list-name-align')
+        cy.get('#user-list-name')
+        .should('have.text', userName)
+      })
+    })
   })
 })
